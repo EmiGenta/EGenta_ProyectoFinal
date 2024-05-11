b2202ded77d67881bc71e869622579aee28f0390
Emiliano Genta <emigenta@gmail.com>
2024-05-11 18:30:37 -0300

    add: creacion modelo tarjetas

--- project/tarjetas/models.py
+++ project/tarjetas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarjeta(models.Model):
+    """Definicion de Tarjetas"""
+    DEBITO = 'débito'
+    CREDITO = 'crédito'
+    TIPO_CHOICES = [
+        (DEBITO, 'Débito'),
+        (CREDITO, 'Crédito'),
+    ]
+
+    nombre = models.CharField(max_length=25)
+    tipo = models.CharField(max_length=25, choices=TIPO_CHOICES, null=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Tarjeta"
+        verbose_name_plural = "Tarjetas"
