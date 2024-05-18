fcc5ee9450823ad4e2eca10aae1c7a6e88f6413e
Emiliano Genta <emigenta@gmail.com>
2024-05-18 18:17:06 -0300

    feat: creacion de registro + actualizacion vista

--- project/core/views.py
+++ project/core/views.py
@@ -1,24 +1,25 @@
-from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
 from .forms import CustomAuthenticationForm, CustomUserCreationForm
 
+User = get_user_model()
 
 # @login_required
 def home(request):
     return render(request, "core/index.html")
 
-
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
     template_name = "core/login.html"
 
-
-@staff_member_required
 def register(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return redirect('core:home')
+    
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
