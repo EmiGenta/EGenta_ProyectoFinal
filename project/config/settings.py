546810ff6c1cace03380ca9641db804b0006a06e
Emiliano Genta <emigenta@gmail.com>
2024-05-18 15:51:44 -0300

    feat: log in log out register

--- project/config/settings.py
+++ project/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b%6_s-+zjv_*13p)kh0yf=0qvqyo(v_u8o6z$d-d74ik3f(m4#'
+#SECRET_KEY = 'django-insecure-b%6_s-+zjv_*13p)kh0yf=0qvqyo(v_u8o6z$d-d74ik3f(m4#'
+
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
