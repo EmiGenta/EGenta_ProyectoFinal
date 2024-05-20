8d9713705b83ce907ce8ec28eb8c13c9b4bdc6bc
Emiliano Genta <emigenta@gmail.com>
2024-05-19 23:09:13 -0300

    fix registro y login

--- project/core/forms.py
+++ project/core/forms.py
@@ -8,20 +8,13 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ['nombre', 'apellido', 'email', 'pais', 'password1', 'password2']
         help_texts = {k: "" for k in fields}
         widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'pais': forms.TextInput(attrs={'class': 'form-control'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'required': 'required', 'autofocus': 'autofocus', 'autocomplete': 'given-name'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control', 'required': 'required', 'autocomplete': 'family-name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'required': 'required', 'autocomplete': 'email'}),
+            'pais': forms.TextInput(attrs={'class': 'form-control', 'required': 'required', 'autocomplete': 'country'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'required': 'required', 'autocomplete': 'new-password'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'required': 'required', 'autocomplete': 'new-password'}),
         }
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.username = user.email  # Set username to be the email
-        if commit:
-            user.save()
-        return user
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
--- project/core/models.py
+++ project/core/models.py
@@ -1,14 +1,35 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    nombre = models.CharField(max_length=30, blank=True)
-    apellido = models.CharField(max_length=30, blank=True)
-    pais = models.CharField(max_length=30, blank=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True, blank=False, null=False)
+    nombre = models.CharField(max_length=30, blank=False, null=False)
+    apellido = models.CharField(max_length=30, blank=False, null=False)
+    pais = models.CharField(max_length=30, blank=False, null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']  # or include other fields that are required
+    REQUIRED_FIELDS = ['nombre', 'apellido', 'pais']
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
